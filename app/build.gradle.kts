f67d99b86b58e9f33155919eee2a415f40c5c992
HugeFrog24 <62775760+HugeFrog24@users.noreply.github.com>
2024-10-22 01:00:30 +0200

    kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    sourceSets {
+        getByName("main") {
+            java.srcDir("src/main/kotlin")
+        }
+    }
 }
 
 dependencies {
